d1ecfa7e84e454d5c35530cc5ae17efeeb9648cc
wrazik <wojciech.razik@gmail.com>
2023-01-26 07:52:28 +0100

    Reed switch support

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,27 @@
 #![no_std]
 #![no_main]
 
-use core::cell::RefCell;
 use core::borrow::BorrowMut;
-use esp32_hal::gpio::{Event, Gpio32, Input, PullDown};
+use core::cell::RefCell;
 use critical_section::Mutex;
+use esp32_hal::gpio::{Event, Gpio25, Gpio32, Input, Output, PullDown, PullUp, PushPull};
 
 use esp32_hal::{
     clock::ClockControl,
     gpio::IO,
-    pac::Peripherals,
+    peripherals::{self, Peripherals},
     prelude::*,
     timer::TimerGroup,
-    Delay,
-    Rtc,
+    Delay, Rtc,
 };
 use esp_backtrace as _;
 use xtensa_lx_rt::entry;
 
 static REED_SWITCH: Mutex<RefCell<Option<Gpio32<Input<PullDown>>>>> = Mutex::new(RefCell::new(None));
-
+static LED: Mutex<RefCell<Option<Gpio25<Output<PushPull>>>>> = Mutex::new(RefCell::new(None));
 #[entry]
 fn main() -> ! {
-    let peripherals = Peripherals::take().unwrap();
+    let peripherals = Peripherals::take();
     let system = peripherals.DPORT.split();
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
 
@@ -45,21 +44,37 @@ fn main() -> ! {
     reed_switch.listen(Event::FallingEdge);
 
     led.set_high().unwrap();
-    critical_section::with(|cs| REED_SWITCH.borrow_ref_mut(cs).replace(reed_switch));
-    esp32_hal::interrupt::enable(esp32_hal::pac::Interrupt::GPIO, esp32_hal::interrupt::Priority::Priority2).unwrap();
+    critical_section::with(|cs| {
+        REED_SWITCH.borrow_ref_mut(cs).replace(reed_switch);
+        LED.borrow_ref_mut(cs).replace(led);
+    });
+
+    esp32_hal::interrupt::enable(
+        peripherals::Interrupt::GPIO,
+        esp32_hal::interrupt::Priority::Priority2,
+    )
+    .unwrap();
 
     // Initialize the Delay peripheral, and use it to toggle the LED state in a
     // loop.
     let mut delay = Delay::new(&clocks);
 
     loop {
-        esp_println::println!("Debug");
-        led.toggle().unwrap();
-        delay.delay_ms(500u32);
     }
 }
 
+#[ram]
 #[interrupt]
 fn GPIO() {
 
+    critical_section::with(|cs| {
+        REED_SWITCH
+            .borrow_ref_mut(cs)
+            .borrow_mut()
+            .as_mut()
+            .unwrap()
+            .clear_interrupt();
+
+        LED.borrow_ref_mut(cs).borrow_mut().as_mut().unwrap().toggle().unwrap();
+    });
 }
